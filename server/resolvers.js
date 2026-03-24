68dd73eccbe5a34f63f0c8c99c0c6a0e4f6a851c
Stefan Radenkovic <stefanthadeus@gmail.com>
2026-03-24 16:25:11 +0100

    Implement job by company fetching

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,4 @@
-import { getJob, getJobs } from "./db/jobs.js";
+import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
 
 export const resolvers = {
@@ -7,6 +7,8 @@ export const resolvers = {
     job: (_root, { id }) => getJob(id),
     jobs: async () => getJobs(),
   },
+
+  Company: { jobs: (company) => getJobsByCompany(company.id) },
 
   Job: {
     company: (job) => getCompany(job.companyId),
