bf6dd5ec90ba58daf3e6d9b6f36e44938f417bc2
Stefan Radenkovic <stefanthadeus@gmail.com>
2026-03-24 16:06:01 +0100

    Implement data fetching on frontend

---
+++ client/src/lib/graphql/queries.js
@@ -0,0 +1,44 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const client = new GraphQLClient("http://localhost:9000/graphql");
+
+export async function getJob(id) {
+  const query = gql`
+    query ($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        date
+        description
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { job } = await client.request(query, { id });
+
+  return job;
+}
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { jobs } = await client.request(query);
+
+  return jobs;
+}
